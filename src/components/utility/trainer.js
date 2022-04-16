7dec4dfee95c16a5190906c8acca230669b6ae1b
hmrahim <h.m.rahimnet@gmail.com>
2022-04-17 02:32:14 +0600

    trainer fake data create done

---
+++ src/components/utility/trainer.js
@@ -0,0 +1,44 @@
+const trainer = [
+    {
+        id:1,
+        name:'Samanta Crane',
+        desig:"Internist, General Practitoner",
+        exp:"Working Since 2004",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBXeURVdTKXjz7JddqQNo4MJO1c1OX5e39Wg&usqp=CAU"
+    },
+    {
+        id:2,
+        name:'Ostin Green',
+        desig:"Internist, General Practitoner",
+        exp:"Working Since 2004",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFaWjdIup4_BU48Hh5IqxnqjF_JVueJDoocQ&usqp=CAU"
+    },
+    {
+        id:3,
+        name:'Norman Colins',
+        desig:"SALES MANAGER",
+        exp:"Working Since 2004",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY2BpG_BwgtY-qkyaKWkpztV1hNT2ARQqpxQ&usqp=CAU"
+    },
+    {
+        id:4,
+        name:'Kazi Hayat',
+        desig:"Junior Trainer",
+        exp:"Working Since 2015",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6kMFz6jwQw-sT0nc1uRQdEbd1um4_8_hENw&usqp=CAU"
+    },
+    {
+        id:5,
+        name:'Saroar alam',
+        desig:"Trainer",
+        exp:"Working Since 2010",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRX1A30KlXvnUU2KZ9IPrDTtaGL-XD2cM3Iig&usqp=CAU"
+    },
+    {
+        id:6,
+        name:'Nabin tapa',
+        desig:"co-trainer",
+        exp:"Working Since 2018",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST20Igo4k_-r2YCB3M3UuSwqvTi0HGfZkahg&usqp=CAU"
+    },
+]
