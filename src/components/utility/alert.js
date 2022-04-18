c30dc6ee6df63fa068820773258f83155ac924f3
hmrahim <h.m.rahimnet@gmail.com>
2022-04-18 17:08:01 +0600

    alert for payment conformation is done

---
+++ src/components/utility/alert.js
@@ -0,0 +1,41 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const sweetAlert = ()=> {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+          confirmButton: 'btn btn-success',
+          cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: true
+      })
+      
+      swalWithBootstrapButtons.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Confirm',
+        cancelButtonText: 'cancel',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Payment Complete',
+            'Your payment has been completed.',
+            'success'
+          )
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelled',
+            'Payment  cancelled:)',
+            'error'
+          )
+        }
+      })
+}
+
+export default sweetAlert
